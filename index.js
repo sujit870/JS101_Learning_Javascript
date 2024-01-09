3453740cf36eda22f1746939f7ac5f0f036fa53f
sujit870 <sunil84168708@gmail.com>
2024-01-09 06:40:36 +0000

    js assignment

---
+++ index.js
@@ -0,0 +1,58 @@
+// Problem 1
+// Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+console.log("Masai school");
+console.log("A Transformation in Education");
+
+// Problem 2
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let myname1 = "sujeet";
+console.log(myname1)
+let father_name = "jayram"
+console.log(father_name)
+//  new
+let mother_name = "tilaki"
+console.log(mother_name)
+
+// Problem 3
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Concatenate and console them in a new string to get the output like this : ("My name is"+ name +"and my age is"+ age +".")
+
+let myname = "sujeet"
+
+let age = 22
+
+console.log(myname,age)
+console.log((typeof myname),(typeof age))
+
+// Problem 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+console.log("🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴")
+console.log("🟡      MMD PUBLIC SCHOOL       🔵")
+console.log("🟡      ▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪       🔵")
+console.log("🟡          RESULT              🔵")
+console.log("🟡 ▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪🔵")
+console.log("🟡                              🔵")
+console.log("🟡      Name- sujeet kumar      🔵")
+console.log("🟡▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪🔵")
+console.log("🟡 Grade- X           Section-B 🔵")
+console.log("🟡▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪🔵")
+console.log("🟡 Roll No- 221684              🔵")
+console.log("🟡▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪🔵")
+console.log("🟡 Subject ❗Total ❗ obt ❗ Pcnt ❗🔵")
+console.log("🟡 Math    ❗100   ❗ 85  ❗ 85%  ❗🔵")
+console.log("🟡 English ❗100   ❗ 66  ❗ 55%  ❗🔵")
+console.log("🟡 Hindi   ❗100   ❗ 85  ❗ 85   ❗🔵")
+console.log("🟡▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪🔵")
+console.log("🟡Total    ❗500   ❗ 288 ❗ 76%  ❗🔵")
+console.log("🟡▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪🔵")
